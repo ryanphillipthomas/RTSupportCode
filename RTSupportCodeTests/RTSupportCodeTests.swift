d692f3d3320074122d1448739a1bd3af73161484
Ryan <ryan.thomas@zolabooks.com>
2017-01-29 15:11:18 -0500

    Initial Commit

---
+++ RTSupportCodeTests/RTSupportCodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  RTSupportCodeTests.swift
+//  RTSupportCodeTests
+//
+//  Created by Ryan Thomas on 10/14/16.
+//  Copyright © 2016 ryanphillipthomas. All rights reserved.
+//
+
+import XCTest
+@testable import RTSupportCode
+
+class RTSupportCodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
